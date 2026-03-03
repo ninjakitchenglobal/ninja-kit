da1a978fe050cbef37a6c5e6f106253b4fefb8fb
ninjakitchenglobal <ninjakitchenglobal@gmail.com>
2026-03-03 05:57:55 +0100

    Update ProductCard.tsx

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -17,12 +17,14 @@ const ProductCard = ({
   return (
     <Link to={`/product/${productId}`}>
       <div className="w-64 h-90 flex flex-col justify-between gap-3 transition-all duration-300 hover:scale-105 hover:shadow-lg cursor-pointer">
-        <span className="block h-80 relative">
+        <span className="block h-70 relative">
           <img src={picture} alt="" className="w-full h-full" />
         </span>
 
         <span>
-          <h3 className="font-semibold text-lg text-gray-800">{title}</h3>
+          <h3 className="font-semibold text-lg text-gray-800">
+            {title.slice(0, 25) + '...'}
+          </h3>
           <p className="mt-2 text-gray-700">${price}</p>
         </span>
       </div>
